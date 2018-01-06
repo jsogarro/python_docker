7382f92a6ff603e42aaa4540b9f859ecbcd53e3b
Jamal O'Garro <jamal.ogarro[at]gmail.com>
2018-01-06 09:20:32 -0500

    add Docker Machine scripts

---
+++ scripts/setup.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+docker-machine create --driver virtualbox default
+docker-machine env default
+eval $(docker-machine env default)
+docker pull ubuntu
---
+++ scripts/start.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+docker-machine start default
